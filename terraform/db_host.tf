cbf35655d9be7a8ddc37904f2aab3c307085b428
Andrey Ivanov <andrey.i@reviewapp.today>
2023-11-23 20:21:29 +0500

    env var

--- terraform/db_host.tf
+++ terraform/db_host.tf
@@ -22,6 +22,7 @@ variable "private_key_path" {
   description = "Path to ssh private key, which would be used to access workers"
   default     = "~/.ssh/id_rsa"
 }
+
 resource "yandex_compute_instance" "bingo-db" {
   platform_id        = "standard-v2"
   service_account_id = yandex_iam_service_account.service-accounts["sagrityanin1"].id
@@ -45,6 +46,7 @@ resource "yandex_compute_instance" "bingo-db" {
     }
   }
   metadata = {
+    test-var: "${var.POSTGRES_DB}"
     docker-compose = templatefile(
       "${path.module}/../db_host/docker-compose.yaml",
       {
@@ -54,16 +56,7 @@ resource "yandex_compute_instance" "bingo-db" {
     )
     ssh-keys  = "ubuntu:${file("~/.ssh/id_rsa.pub")}"     
   }
-  provisioner "file" {
-    source      = "../db_host/.env"
-    destination = "/home/ubuntu/.env"
-    connection {
-      type     = "ssh"
-      user     = "ubuntu"
-      private_key = file(var.private_key_path)
-      host = self.network_interface[0].nat_ip_address
-    }
-  }
+  
   provisioner "file" {
     source      = "../db_host/docker-compose.yaml"
     destination = "/home/ubuntu/docker-compose.yaml"
@@ -83,11 +76,18 @@ resource "yandex_compute_instance" "bingo-db" {
     }
     inline = [
       "cd /home/ubuntu",
-      "export $(cat .env)",
+      "echo 'POSTGRES_PASSWORD=123qwe' | sudo tee -a /etc/environment",
+      "echo 'POSTGRES_USER=andrey' | sudo tee -a /etc/environment",
+      "echo 'POSTGRES_DB=bingo' | sudo tee -a /etc/environment",
+      "echo 'POSTGRES_HOST=bingo_db' | sudo tee -a /etc/environment",
+      "echo 'POSTGRES_PORT=5432' | sudo tee -a /etc/environment",
       "env",
       "echo 'export env done'",
-      "sleep 20",
-      "docker images -a"
+      "sleep 60",
+      "docker images -a",
+      "docker system prune -f",
+      "docker-compose up -d"
+      
     ]
   }
 }
