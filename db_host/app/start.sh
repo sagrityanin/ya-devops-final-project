61c28e072a2d47c3f1a80f5879e897d942d11973
Andrey Ivanov <andrey@dynfor.ru>
2023-11-23 10:06:42 +0500

    Db host (#6)
    * readme
    
    * init db
    
    * docker compose ya image
    
    ---------
    
    Co-authored-by: Andrey Ivanov <andrey.i@reviewapp.today>

---
+++ db_host/app/start.sh
@@ -0,0 +1,21 @@
+
+cat > /opt/bingo/config.yaml <<EOF
+student_email: andrey@dynfor.ru
+postgres_cluster:
+  hosts:
+  - address: $POSTGRES_HOST
+    port: $POSTGRES_PORT
+  user: $POSTGRES_USER
+  password: $POSTGRES_PASSWORD
+  db_name: $POSTGRES_DB
+  ssl_mode: disable
+  use_closest_node: false
+EOF
+echo "Create config.yaml"
+cd /app
+sleep 10
+echo "Start prepare db"
+./bingo prepare_db
+export PGPASSWORD=$POSTGRES_PASSWORD
+psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER \
+-d $POSTGRES_DB < create_index.sql
