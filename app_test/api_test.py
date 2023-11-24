76d5dc23811d8cf65a35d88bd76835e060040cdb
Andrey Ivanov <andrey.i@reviewapp.today>
2023-11-24 10:26:20 +0500

    app image

--- app_test/api_test.py
+++ app_test/api_test.py
@@ -4,7 +4,7 @@ from time import time
 start_time = time()
 
 for i in range(1000):
-    url = f"http://127.0.0.1:32531/api/customer/{i}"
+    url = f"http://127.0.0.1:8000/api/customer/{i}"
     res = requests.get(url)
     if res.status_code != 200:
         print(res.status_code)
